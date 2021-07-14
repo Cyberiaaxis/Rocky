44820aad49ef1e4513da363cf4316a9b080b3c79
Rocky <cyberiaaxis@gmail.com>
2021-07-14 04:01:47 +0000

    css of stats

--- resources/js/components/Stats.js
+++ resources/js/components/Stats.js
@@ -1,209 +1,81 @@
 import React, { useState } from "react";
-import Chart from "react-google-charts";
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import "../styles/Stats.scss";
 
-const useStyles = makeStyles((theme) => ({
-  popover: {
-    pointerEvents: 'none',
-  },
-  paper: {
-    padding: theme.spacing(1),
-  },
-}));
-
 const Stats = () => {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    return (
+        <React.Fragment>
+        <div class="StatsBox">
+            <div>
+                <ul class="statsDetails">
+                    <li>
+                        <h2>First Stat</h2>
+                        <ul>
+                            <li>second item first subitem</li>
+                            <li>second item second subitem</li>
+                            <li>second item third subitem</li>
+                        </ul>
+                    </li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="statsDetails">
+                    <li>
+                        <h2>Second Stat</h2>
+                        <ul>
+                            <li>second item first subitem</li>
+                            <li>second item second subitem</li>
+                            <li>second item third subitem</li>
+                        </ul>
+                    </li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                </ul>
+            </div>
+        </div>
+        <div class="StatsBox">
+            <div>
+                <ul class="statsDetails">
+                    <li>
+                        <h2>First Stat</h2>
+                        <ul>
+                            <li>second item first subitem</li>
+                            <li>second item second subitem</li>
+                            <li>second item third subitem</li>
+                        </ul>
+                    </li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="statsDetails">
+                    <li>
+                        <h2>Second Stat</h2>
+                        <ul>
+                            <li>second item first subitem</li>
+                            <li>second item second subitem</li>
+                            <li>second item third subitem</li>
+                        </ul>
+                    </li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                    <li><p>Total</p></li>
+                </ul>
+            </div>
+        </div>
+        </React.Fragment>
 
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
+    );
+};
 
-  const open = Boolean(anchorEl);
-
-  return(
-    <React.Fragment>
-      <div className="StatsBox">
-      <div>
-        <ul>
-          <li>
-          <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
-      >
-        Hover with a Popover.
-      </Typography>
-      <Popover
-        id="mouse-over-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography>I use Popover.</Typography>
-      </Popover>
-          </li>
-            <ol>
-              <li>second item first subitem</li>
-              <li>second item second subitem</li>
-              <li>second item third subitem</li>
-            </ol>
-          <li>Total</li>
-        </ul>
-     </div>
-     <div>
-        <ul>
-        <li>
-          <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
-      >
-        Hover with a Popover.
-      </Typography>
-      <Popover
-        id="mouse-over-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography>I use Popover.</Typography>
-      </Popover>
-          </li>
-
-            <ol>
-              <li>second item first subitem</li>
-              <li>second item second subitem</li>
-              <li>second item third subitem</li>
-            </ol>
-          <li>Total</li>
-        </ul>
-     </div>
-      </div>
-
-      <div className="StatsBox">
-      <div>
-        <ul>
-        <li>
-          <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
-      >
-        Hover with a Popover.
-      </Typography>
-      <Popover
-        id="mouse-over-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography>I use Popover.</Typography>
-      </Popover>
-          </li>
-
-            <ol>
-              <li>second item first subitem</li>
-              <li>second item second subitem</li>
-              <li>second item third subitem</li>
-            </ol>
-          <li>Total</li>
-        </ul>
-     </div>
-     <div>
-        <ul>
-        <li>
-          <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
-      >
-        Hover with a Popover.
-      </Typography>
-      <Popover
-        id="mouse-over-popover"
-        className={classes.popover}
-        classes={{
-          paper: classes.paper,
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography>I use Popover.</Typography>
-      </Popover>
-          </li>
->
-            <ol>
-              <li>second item first subitem</li>
-              <li>second item second subitem</li>
-              <li>second item third subitem</li>
-            </ol>
-          <li>Total</li>
-        </ul>
-     </div>
-      </div>
-
-    </React.Fragment>
-  );
-}
 export default Stats;
